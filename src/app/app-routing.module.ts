d9ed7dc26117cb4a5330c176a7240b5557c6fce4
kullervo21 <kullervo21@gmail.com>
2019-09-19 08:35:47 +0200

    essai

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListeProduitComponent} from './liste-produit/liste-produit.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {SignUpFormComponent} from './sign-up-form/sign-up-form.component';
+import {LoginFormComponent} from './sign-in-form/login-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'produits', component: ListeProduitComponent },
+  { path: 'inscription', component: SignUpFormComponent },
+  { path: 'identification', component:  LoginFormComponent },
+  {
+    path: '',
+    redirectTo: '/produits',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,50 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { SignInUpComponent } from './sign-in-up-button/sign-in-up.component';
+import { MenuComponent } from './menu/menu.component';
+import { ListeProduitComponent } from './liste-produit/liste-produit.component';
+import {HttpClientModule} from '@angular/common/http';
+import {
+  _MatMenuDirectivesModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  MatTableModule
+} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginFormComponent } from './sign-in-form/login-form.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInUpComponent,
+    MenuComponent,
+    ListeProduitComponent,
+    SignUpFormComponent,
+    PageNotFoundComponent,
+    LoginFormComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    _MatMenuDirectivesModule,
+    MatMenuModule,
+    ReactiveFormsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
